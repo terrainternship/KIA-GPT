60216b37b94a9a9bc99e47aef6f8f76fa700643b
Home-ПК\Home <musicnova@yandex.ru>
2023-10-25 16:25:01 +0300

    test/ISSUE-4_new_baza added parser (2)

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,6 +1,8 @@
 from step00_create_excel import step00_create_excel
 from step01_build_excel import step01_build_excel
+from step02_parse_website import step02_parse_website
 
 if __name__ == '__main__':
     step00_create_excel().run("step00_create_excel")
     step01_build_excel().run("step01_build_excel")
+    step02_parse_website().run("step02_parse_website")
