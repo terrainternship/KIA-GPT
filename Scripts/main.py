1b48fb13d2eb1bf66338873c94e725c042d8784e
Home-ПК\Home <musicnova@yandex.ru>
2023-10-25 15:58:16 +0300

    test/ISSUE-4_new_baza init commit

---
+++ Scripts/main.py
@@ -0,0 +1,6 @@
+from step00_create_excel import step00_create_excel
+from step01_build_excel import step01_build_excel
+
+if __name__ == '__main__':
+    step00_create_excel().run("step00_create_excel")
+    step01_build_excel().run("step01_build_excel")
