a74e4878a6cdf83fb93588d5ed73795d920f483f
Home-ПК\Home <musicnova@yandex.ru>
2023-10-25 16:42:44 +0300

    test/ISSUE-4_new_baza added parser_database

---
+++ Scripts/step03_append_website.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+class step03_build_website():
+    def __init__(self):
+        pass
+
+    def run(self, msg):
+        # 1. Автомобили Kia в кредит - расчитать кредит на покупку https://www.kia.ru/buy/calc/
+        # 2. Расчет стоимости ТО - https://www.kia.ru/service/calculator_to/
+        # 3. Программа Trade-In - https://www.kia.ru/buy/trade-in/
+        EXCEL_TEXT = ""
+        import configparser
+        import pathlib
+        import os
+        config = configparser.ConfigParser()
+        config.read('config.txt')
+        knowledge_dir = config["DEFAULT"]["knowledge_dir"]
+        if knowledge_dir is None: knowledge_dir = "./knowledge"
+        pathlib.Path(knowledge_dir).mkdir(parents=True, exist_ok=True)
+        filepath_01 = os.path.join(knowledge_dir, "__01_calc_credit_database.txt")
+        with open(filepath_01, "w") as f01:
+            f01.write("")
+        filepath_02 = os.path.join(knowledge_dir, "__02_calc_tech_database.txt")
+        with open(filepath_02, "w") as f02:
+            f02.write("")
+        filepath_03 = os.path.join(knowledge_dir, "__03_calc_tradein.txt")
+        with open(filepath_03, "w") as f03:
+            f03.write("")
+        print(msg, " ... OK")
+
+if __name__ == '__main__':
+    step01_build_excel().run("")
