265464615f2c59429e8c1cbde6834044ff8776bf
musicnova <musicnova@yandex.ru>
2023-10-30 15:50:44 +0300

    Update main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,10 +1,10 @@
 from step00_create_excel import step00_create_excel
-from step01_copy_excel import step01_build_excel
+from step01_copy_excel import step01_copy_excel
 from step02_parse_website import step02_parse_website
-from step03_copy_website import step03_append_website
+from step03_copy_website import step03_copy_website
 
 if __name__ == '__main__':
     step00_create_excel().run("step00_create_excel")
-    step01_build_excel().run("step01_build_excel")
+    step01_copy_excel().run("step01_copy_excel")
     step02_parse_website().run("step02_parse_website")
-    step03_append_website().run("step03_append_website")
+    step03_copy_website().run("step03_copy_website")
