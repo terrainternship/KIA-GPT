e9a30614982b0c7a911b6839c7bbc24bd7c782fd
Home-ПК\Home <musicnova@yandex.ru>
2023-10-30 19:13:21 +0300

    up Scripts/main.py

---
+++ Scripts/step07_copy_video.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class step07_copy_video():
+    def __init__(self):
+        pass
+
+    def run(self, msg):
+
+        print(msg, " ... ERROR")
+
+if __name__ == '__main__':
+    step07_copy_video().run("")
