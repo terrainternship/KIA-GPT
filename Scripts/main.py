492b9fb734bad483512c583a1de36979659b4276
Home-ПК\Home <musicnova@yandex.ru>
2023-10-30 17:13:14 +0300

    added pdf step04

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,9 +2,11 @@ from step00_create_excel import step00_create_excel
 from step01_copy_excel import step01_copy_excel
 from step02_parse_website import step02_parse_website
 from step03_copy_website import step03_copy_website
+from step04_parse_pdf import step04_parse_pdf
 
 if __name__ == '__main__':
     step00_create_excel().run("step00_create_excel")
     step01_copy_excel().run("step01_copy_excel")
     step02_parse_website().run("step02_parse_website")
     step03_copy_website().run("step03_copy_website")
+    step04_parse_pdf().run("step04_parse_pdf")
