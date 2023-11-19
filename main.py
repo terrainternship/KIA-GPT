d65311baef8a6755efb2dfc5d6783a425fa1bebb
Alex M. <johndoe@example.com>
2023-11-19 15:35:54 +0700

    переместил insert_newlines в gpt.py

--- main.py
+++ main.py
@@ -17,26 +17,13 @@ def clear_screen():
         os.system('clear')
 
 
-def insert_newlines(textstr: str, max_len: int = 170) -> str:
-    words = textstr.split()
-    lines = []
-    current_line = ""
-    for word in words:
-        if len(current_line + " " + word) > max_len:
-            lines.append(current_line)
-            current_line = ""
-        current_line += " " + word
-    lines.append(current_line)
-    return "\n".join(lines)
-
-
 def run_dialog():
     while True:
         user_question = input('\nВопрос: ')
         if ((user_question.lower() == 'stop') or (user_question.lower() == 'стоп')):
             break
         answer = OpenGPT.answer_index(user_question)
-        print('\nОтвет: ', insert_newlines(answer)+'')
+        print('\nОтвет: ', OpenGPT.insert_newlines(answer)+'')
 
     return
 
